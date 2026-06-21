4d5f6f6ae1423fd9145131a006f0355e4209096b
Chuma Humphrey <chumphrey17c@gmail.com>
2026-06-21 11:19:24 -0500

    baisc JS testing for parser complete

--- qa-gen-frontend/src/utils/__tests__/parser.test.jsx
+++ qa-gen-frontend/src/utils/__tests__/parser.test.jsx
@@ -1,5 +1,6 @@
 import { screen } from "@testing-library/dom";
 import { parseAndMapQuestions } from "../parser.js";
+import {backgroundRepeat} from "jsdom/lib/generated/css-property-descriptors.js";
 
 describe('parseAndMapQuestions', () => {
 
@@ -76,6 +77,40 @@ describe('parseAndMapQuestions', () => {
 
         // Arrange
         const rawInputText = 'What is the capital of France?';
+
+        // Act
+        const result = parseAndMapQuestions(rawInputText);
+
+        // Assert
+        expect(result.success).toBe(false);
+        expect(result.errors).toEqual([
+            "No valid questions found. Check text formatting."
+        ]);
+        expect(result.data).toBe(null);
+    });
+
+    it('should throw an error if there is not sequential lettering (A, B, C, ...)', () => {
+        // Arrange
+        const rawInputText = '1. What is the capital of France?\n' +
+            'A. Berlin\n' +
+            'C. Madrid*\n' +
+            'C. Paris\n' +
+            'D. Rome';
+
+        // Act
+        const result = parseAndMapQuestions(rawInputText);
+
+        // Assert
+        expect(result.success).toBe(false);
+        expect(result.errors).toEqual([
+            "Question 1: Lettering not sequential. Expected B."
+        ]);
+        expect(result.data).toBe(null);
+    });
+
+    it('should not find any valid questions if user submits random information', () => {
+        // Arrange
+        const rawInputText = "Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id. Placerat in id cursus mi pretium tellus duis. Pretium tellus duis convallis tempus leo eu aenean."
 
         // Act
         const result = parseAndMapQuestions(rawInputText);
